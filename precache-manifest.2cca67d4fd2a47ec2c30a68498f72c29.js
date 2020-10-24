7aebbfd3892d9223b42649b4a87e88efe8cac202
honey bee <honeybee@users.noreply.github.com>
2020-10-24 19:20:34 -0400

    Updates

---
+++ precache-manifest.2cca67d4fd2a47ec2c30a68498f72c29.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b51a5db469b283d1b4edf975059cb0ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdec6c05d83352b3dc61",
+    "url": "/static/css/main.74e7798a.chunk.css"
+  },
+  {
+    "revision": "8801bb9bf9df1a5c6a76",
+    "url": "/static/js/2.bff3e6d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bff3e6d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdec6c05d83352b3dc61",
+    "url": "/static/js/main.ee9dc082.chunk.js"
+  },
+  {
+    "revision": "35839d639db8e60032a1",
+    "url": "/static/js/runtime-main.f78ad96b.js"
+  },
+  {
+    "revision": "101b002cb25f6af6a18487e29e4e9e6f",
+    "url": "/static/media/alain.101b002c.jpg"
+  },
+  {
+    "revision": "d82ecb922acc1b4ef0f9ca0383849d79",
+    "url": "/static/media/alina.d82ecb92.jpg"
+  },
+  {
+    "revision": "77975781fa9cddaa5d1ddfab536e4379",
+    "url": "/static/media/angie.77975781.jpg"
+  },
+  {
+    "revision": "0491bd43bf684e760992f07dd6043cfd",
+    "url": "/static/media/ike.0491bd43.jpg"
+  },
+  {
+    "revision": "45391aa0238c0512b180b2157eb6f047",
+    "url": "/static/media/judah.45391aa0.jpg"
+  },
+  {
+    "revision": "93abf2f7d631663535b9c9f73b429c2c",
+    "url": "/static/media/marly.93abf2f7.jpg"
+  },
+  {
+    "revision": "2153540e71af2882c5a9b7ae6f16dcae",
+    "url": "/static/media/osmund.2153540e.jpg"
+  },
+  {
+    "revision": "11a9ee82944eac101c8596f73a84b686",
+    "url": "/static/media/penn.11a9ee82.jpg"
+  },
+  {
+    "revision": "ebc8ea2bb46a6226478a586ba42287a3",
+    "url": "/static/media/tabbatha.ebc8ea2b.jpg"
+  }
+]);
